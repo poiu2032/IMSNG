1cb9c04a7aa6b4e267f8db0afc51e4e48da0b9c5
poiu2032 <sylee@astro.snu.ac.kr>
2017-04-06 02:37:36 +0900

    Update 30inch_imagefill.py

--- 30inch_imagefill.py
+++ 30inch_imagefill.py
@@ -30,6 +30,15 @@ def fill_im(im):
 	datasec=hdr['datasec']
 	ccdsec=hdr['ccdsec']
 
+	hdr['naxis1']=long(2080)
+        hdr['naxis2']=long(2048)
+        hdr['biassec']='[2057:2072,2:2047]'
+        hdr['trimsec']='[2:2047,2:2047]'
+        hdr['datasec']='[1:2048,1:2048]' # it must be changed, other parameter is irrelevant but just for mtching with full frame image's header
+        hdr['ccdsec']='[1:2048,1:2048]'
+
+		
+	
 	ccdsecx1=int(ccdsec[1:-1].split(',')[0].split(':')[0])
 	ccdsecx2=int(ccdsec[1:-1].split(',')[0].split(':')[1])
 	ccdsecy1=int(ccdsec[1:-1].split(',')[1].split(':')[0])
